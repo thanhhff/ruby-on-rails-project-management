828e223128bb34039fd4311843e46e29ba602da3
Thanhhff <thanhnguyen200399@gmail.com>
2020-11-29 00:15:33 +0700

    Fix team models

---
+++ db/migrate/20201128171507_create_activities.rb
@@ -0,0 +1,6 @@
+class CreateActivities < ActiveRecord::Migration[6.0]
+  def change
+    create_table :activities do |t|
+    end
+  end
+end
--- db/schema.rb
+++ db/schema.rb
@@ -11,6 +11,8 @@
 # It's strongly recommended that you check this file into your version control system.
 
 ActiveRecord::Schema.define(version: 2020_11_28_103037) do
+  # These are extensions that must be enabled in order to support this database
+  # enable_extension "plpgsql"
 
   create_table "projects", force: :cascade do |t|
     t.string "name"
